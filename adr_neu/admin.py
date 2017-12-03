174261b7900bb5768fa4e7e1cc3247b2d38aa703
Gernot Hillier <gernot.hillier@siemens.com>
2017-12-03 21:41:54 +0100

    Admin: GIS_STATUS nicht mehr anzeigen

--- adr_neu/admin.py
+++ adr_neu/admin.py
@@ -10,7 +10,7 @@ class HausnummerInline(admin.TabularInline):
 class HausnummerAdmin(admin.ModelAdmin):
 	list_filter = ['strasse__stadtteil']
 	search_fields = ['strasse__name', 'nummer']
-	fields = ('strasse', 'nummer',('laenge', 'breite'), ('status', 'kommentar'),'gis_status' )
+	fields = ('strasse', 'nummer',('laenge', 'breite'), ('status', 'kommentar') )
 
 class StrasseInline(admin.TabularInline):
 	model = Strasse
